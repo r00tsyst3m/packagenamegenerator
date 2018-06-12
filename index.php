550cdd0fc3a6fc0bdc921d8f4d1bbbd16fb5544e
r00tsyst3m <muliawan.daffa@gmail.com>
2018-06-12 17:46:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ use iamroot_play_parse\playstore;
 
 $push = new playstore();
 $push->keyword = ("a");
+//misal keyword 'rpg' maka muncul aplikasi yg terkait rpg 
 $push->page = (1);
+// jumlah package_name per page = 15-30
 $push = $push->brute_force();
 
 
